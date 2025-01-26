9bed89f3ac1e6436c33875a945657db8a63e467f
Sergi Botargues <sbotargues@gmail.com>
2025-01-26 18:56:42 +0100

    maquetar

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
         />
       </head>
       <body>
